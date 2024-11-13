e54ffb27cb9de5e2c5fa6c67c6e567ab9b0250ff
Eason <orz0830@gmail.com>
2024-11-14 02:16:35 +0800

    feat:下載 entity framework 建立 DbModel enable-migration add-migration update-database
    feat:更改 webconfig 的資料庫 source

---
+++ MVC_20241114/Migrations/202411131806175_AddMemberModel.cs
@@ -0,0 +1,28 @@
+﻿namespace MVC_20241114.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMemberModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Members",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Account = c.String(nullable: false, maxLength: 20),
+                        Password = c.String(nullable: false, maxLength: 20),
+                        NewPermission = c.String(maxLength: 500),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Members");
+        }
+    }
+}
---
+++ MVC_20241114/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace MVC_20241114.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MVC_20241114.Models.DbModel>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MVC_20241114.Models.DbModel context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
