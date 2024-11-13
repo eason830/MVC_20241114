1737ef427487cfb81572af893d868023f992a757
Eason <orz0830@gmail.com>
2024-11-14 00:34:08 +0800

    feat:新增 Member.cs Model
    feat:引入套板使用的 html css js

---
+++ MVC_20241114/Models/Member.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_20241114.Models
+{
+    public class Member
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "編號")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "帳號")]
+        [MaxLength(20)]
+        public string Account { get; set; }
+
+        [Required]
+        [Display(Name = "密碼")]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        [Display(Name = "權限")]
+        [MaxLength(500)]
+        public string NewPermission { get; set; }
+
+    }
+}
